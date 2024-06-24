d1cee24a37667592bfcc9f22e36fffae8e2a3fb2
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-06-24 15:12:45 +0300

    add contacts

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
 
-from catalog.views import home
+from catalog.views import home, contacts
 
 urlpatterns = [
     path('', home),
+    path('contacts/', contacts),
 ]
