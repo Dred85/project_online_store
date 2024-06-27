1d7a7f31c5682b82ab7318f798dc70a2aab3e451
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-06-27 07:03:09 +0300

    fix models.py

--- catalog/models.py
+++ catalog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=120)
-    massage = models.CharField(max_length=255)
+    message = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
