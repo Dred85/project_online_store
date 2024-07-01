98333c01e0d0bd0d3350c89a74e0e0f364a96f98
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-07-01 21:01:54 +0300

    add manufactured_at

--- catalog/models.py
+++ catalog/models.py
@@ -21,7 +21,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
-    manufactured_at = models.DateField(auto_now=True)  # Новое поле
+
 
     def __str__(self):
         return self.name
