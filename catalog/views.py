d184c06a618ec1639fce590d3098fb2023252830
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-07-02 19:39:56 +0300

    add forms.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from catalog.forms import ContactForm
 from catalog.models import Product, Contact
 
 
@@ -18,4 +19,13 @@ def home(request):
 
 def contacts(request):
     contact_info = Contact.objects.all()
-    return render(request, 'main/contacts.html', {'contact_info': contact_info})
+    # return render(request, 'main/contacts.html', {'contact_info': contact_info})
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('success')  # Перенаправление на страницу успеха после сохранения
+    else:
+        form = ContactForm()
+    return render(request, 'main/contacts.html', {'form': form})
