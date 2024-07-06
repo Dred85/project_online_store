743c783a6ecb6411754e8630fd5ea474285f5940
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-07-06 23:20:39 +0300

    add my_tags.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 
 from catalog import views
-from catalog.views import home, contacts, products
+from catalog.views import home, contacts, product_detail, catalog, create
+
+# app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('products/', products, name='products'),
-
+    path('products/<int:pk>/', product_detail, name='product_detail'),
+    path('catalog/<int:per_page>/<int:page>/', catalog, name='catalog'),
+    path('products/<int:pk>/<int:per_page>/<int:page>/', product_detail, name='product_detail_paginated'),
+    path('create/', create, name='create')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect
+import os
+from datetime import datetime
+
+from django.shortcuts import render, redirect, get_object_or_404
 
 from catalog.forms import ContactForm
 from catalog.models import Product, Contact, Category
@@ -27,8 +30,106 @@ def contacts(request):
             return redirect('contacts')
     return render(request, 'main/contacts.html', {'contact_info': contact_info})
 
+def catalog(request, page, per_page):
 
-def products(request):
-    contact_info = Contact.objects.all()
+    len_product = len(Product.objects.all())
+    if len_product % per_page != 0:
+        page_count = [x+1 for x in range((len_product // per_page) + 1)]
+    else:
+        page_count = [x + 1 for x in range((len_product // per_page))]
 
-    return render(request, 'main/products.html', {'contact_info': contact_info})
+    product_list = Product.objects.all()[per_page * (page - 1): per_page * page]
+
+
+    context = {
+        "product_list": product_list,
+        "page": page,
+        "per_page": per_page,
+        "page_count": page_count
+    }
+
+
+    return render(request, 'main/per_page.html', context)
+
+def product_detail(request, pk, page=None, per_page=None):
+    _object = get_object_or_404(Product, pk=pk)
+    context = {
+        "object": _object,
+        "pagination": bool(per_page),
+        "per_page": per_page,
+        "page": page
+    }
+
+    return render(request, 'main/product_detail.html', context)
+
+def handle_uploaded_file(f, difference_between_files):
+
+    if os.path.exists(os.path.join(f"media/product/photo/{f.name}")):
+        filename = difference_between_files + f.name
+        print(f"file exists! f.name = {f.name}, new={filename}")
+    else:
+        filename  = f.name
+
+    with open(f"media/product/photo/{filename}", "wb+") as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+        return f'product/photo/{filename}'
+
+def create(request):
+    """
+       Product
+       - Наименование name
+       - Описание description
+       - Изображение (превью) image
+       - Категория category
+       - Цена за покупку price
+       - Дата создания (записи в БД) created_at
+       - Дата последнего изменения (записи в БД) updated_at
+       """
+
+
+
+    number = len(Product.objects.all())
+    if number > 5:
+        products_list = Product.objects.all()[number - 5: number + 1]
+    else:
+        products_list = Product.objects.all()
+
+
+    category_list = Category.objects.all()
+
+
+
+    context = {
+        'object_list': products_list,
+        'category_list': category_list,
+        'title': 'Добавить продукт',
+    }
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        price = request.POST.get('price')
+        category = request.POST.get('category')
+        # image = 'product/photo/' + str(request.POST.get('image'))
+
+        time_of_creation = (datetime.now()).strftime('%Y-%m-%d')
+
+        try:
+            image = handle_uploaded_file(request.FILES['image'], f"{time_of_creation}_{name}_")
+        except:
+            print("Изображения-то нет...")
+            image = None
+
+        info = {'created_at': time_of_creation,
+                'updated_at': time_of_creation,
+                'name': name, 'price': price, 'description': description,
+                'category': Category.objects.get(id=category), 'image': image
+                }
+
+        print(info)
+        Product.objects.create(**info)
+
+
+    return render(request, 'catalog/create_product.html', context)
+
