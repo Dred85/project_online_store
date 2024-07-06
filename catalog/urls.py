687ec7981afb3efe51450feeb04daf04ed44f724
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-07-06 11:24:35 +0300

    add bootstrapp

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,7 +4,7 @@ from catalog import views
 from catalog.views import home, contacts
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
 
 ]
