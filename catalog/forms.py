5c3aa64d0fef0967a04510a82b17deb6465230ed
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-07-29 20:20:36 +0300

    Добавил валидацию и стили через init

--- catalog/forms.py
+++ catalog/forms.py
@@ -21,6 +21,22 @@ class ProductForm(forms.ModelForm):
             'image': forms.FileInput(attrs={'class': 'form-control'}),
         }
 
+    def clean_name(self):
+        PROHIBITED_WORDS = [
+            'казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар'
+        ]
+        cleaned_data = self.cleaned_data['name']
+        for word in PROHIBITED_WORDS:
+            if word in cleaned_data.lower():
+                raise forms.ValidationError("Имя не должно содержать запрещенные слова:'казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар'")
+        return cleaned_data
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
     def save(self, commit=True):
         instance = super().save(commit=False)
         instance.created_at = datetime.now()
