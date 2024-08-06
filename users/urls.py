dd99fea55e69a677ff277811e425d8b8a6a1fe8d
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-08-06 19:56:04 +0300

    Перешел на Django 4.2

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from users.apps import UsersConfig
 from django.contrib.auth.views import LoginView, LogoutView
 
+from users.views import RegisterView
+
 app_name = UsersConfig.name
 
 urlpatterns = [
