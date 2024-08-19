3d0b54aafe70811c743f74f32d4f615278386982
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-08-19 19:21:18 +0300

    fix services.py

--- catalog/services.py
+++ catalog/services.py
@@ -1,20 +1,18 @@
 from django.core.cache import cache
 
 from catalog.models import Category
-from config import settings
+from config.settings import CACHE_ENABLED
 
 
-def get_cached_category(recached: bool = False):
-    if settings.CACHE_ENABLED:
-        key = f"category_list"
-        if recached:
-            category_list = Category.objects.all()
-            cache.set(key, category_list)
-        else:
-            category_list = cache.get(key)
-            if category_list is None:
-                category_list = Category.objects.all()
-                cache.set(key, category_list)
-    else:
-        category_list = Category.objects.all()
-    return category_list
+def get_cached_category():
+    if not CACHE_ENABLED:
+        return Category.objects.all()
+    key = 'category_list'
+    category = cache.get(key)
+    if category is not None:
+        return category
+    category = Category.objects.all()
+    cache.set(key, category)
+    return category
+
+
